0698b0efe397f681e5e407887f5830e748551d2c
Samet Kagan Keskin <sametkagankeskin@gmail.com>
2024-04-20 17:23:07 +0300

    Updated commit line

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int a[] = { 6, 4, 2, 3, 1, 5 };
-        a = SelectionSort.selectionSort(a, 6);
-        for (int i = 0; i < 6; i++)
+        int a[] = { 6, 4, 2, 3, 1, 5, 9 };
+        a = SelectionSort.selectionSort(a, 7);
+        for (int i = 0; i < 7; i++)
             System.out.println(a[i]);
     }
 }
--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -4,17 +4,20 @@ public class SelectionSort {
         int temp; // İki ifadeyi swap etmek için 3. bardağa ihtiyaç vardır
         int min;
 
-        for (int i = 0; i < n - 1; i++) // 1.eleman hariç tüm diziyi gezecek
-        {
-            min = i;
+        // iki tane işaretçi belirleyip bunları karşılaştırarak küçükten büyüğe sıralama
+        // yapıldı
+        for (int i = 0; i < n - 1; i++) {
+            min = i; // aslında birinci işaretçinin indeksi olacak
 
             for (int j = i; j < n - 1; j++) {
+                // ikinci işaretçi (A[j]) ile birinci işaretçi (A[min]) karşılaştırılır
+                // Böylelikle dizideki en küçük elemanın indeksini bulacaktır.
                 if (A[j] < A[min]) {
 
                     min = j;
                 }
 
-            }
+            } // sıralanmış bir dizi olsa da swap işlemi yine de yapılacak (extra cost)
             temp = A[i]; // swap işlemi yapıldı.
             A[i] = A[min];
             A[min] = temp;
